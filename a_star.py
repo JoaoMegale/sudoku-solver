711f6f4c543c44bdcf94145cc3135135629098ab
Joao Megale <joaoluizlopesmegale@gmail.com>
2024-04-24 22:26:18 -0300

    fix a_star code (falta melhorar heuristica)

--- a_star.py
+++ a_star.py
@@ -1,5 +1,6 @@
 import heapq
 from common import is_valid
+import numpy as np
 
 def count_filled_3x3_blocks(tabuleiro):
     """
@@ -49,14 +50,14 @@ def count_filled_segments(board):
     cols = count_filled_columns(board)
     blocks = count_filled_3x3_blocks(board)
     
-    return -1 * (rows + cols + blocks)
+    return 27 - (rows + cols + blocks)
 
 
 def find_best_cell(board):
     """
     Encontra a célula que maximiza a heurística.
     """    
-    max_filled_segments = 0
+    max_filled_segments = 27
     best_cell = None
     
     for i in range(9):
@@ -64,8 +65,8 @@ def find_best_cell(board):
             if board[i][j] == 0:
                 new_board = [row[:] for row in board]
                 new_board[i][j] = 10 # Valor auxiliar
-                filled_segments = count_filled_segments(new_board) * -1
-                if filled_segments >= max_filled_segments:
+                filled_segments = count_filled_segments(new_board)
+                if filled_segments <= max_filled_segments:
                     max_filled_segments = filled_segments
                     best_cell = (i,j)
                     
@@ -88,13 +89,18 @@ def a_star_sudoku(board):
     f = h + g
     
     # Adiciona o tabuleiro e o f iniciais ao heap
-    heapq.heappush(open_heap, (f, board))
+    heapq.heappush(open_heap, (f, g, h, board))
     num_expanded_states = 0
     
     while open_heap:
         # Atualiza o estado de acordo com a fila de prioridade
-        _, current_board = heapq.heappop(open_heap)
+        current_f, current_g, current_h, current_board = heapq.heappop(open_heap)
         num_expanded_states += 1
+
+        print("Board:\n", np.array(current_board))
+
+        if num_expanded_states == 10:
+            return None, 0
 
         # Encontra a melhor célula de acordo com a heurística
         best_cell = find_best_cell(current_board)
@@ -115,15 +121,15 @@ def a_star_sudoku(board):
                 new_board[row][col] = num
                 
                 # Atualiza o custo até então (g)
-                g += h
+                new_g = current_g + 1
                 
                 # Atualiza o valor da heurística
-                h = count_filled_segments(new_board)
+                new_h = count_filled_segments(new_board)
                 
                 # Atualiza o valor de f (g+h)
-                f = g + h
+                new_f = new_g + new_h
                 
                 # Adiciona o estado ao heap, junto com seu valor de f
-                heapq.heappush(open_heap, (f, new_board))
+                heapq.heappush(open_heap, (new_f, new_g, new_h, new_board))
 
     return None, num_expanded_states
